2f6ed0f079c39d57cdbf20cf09cb30c9efdc46ff
Matheus Gonçalves <matheus_gon97@hotmail.com>
2022-11-05 09:14:31 -0300

    conectando a lista de bolões com o backend

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,12 +1,47 @@
-import { VStack, Icon } from 'native-base';
+import { VStack, Icon, useToast, FlatList } from 'native-base';
 import { Octicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+
+import { api } from '../services/api';
 
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+import { EmptyMyPoolList } from '../components/EmptyMyPoolList';
+import { PoolCard, PoolCardProps } from '../components/PoolCard';
+import { EmptyPoolList } from '../components/EmptyPoolList';
+
 
 export function Pools(){
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardProps[]>([]);
+  const toast = useToast();
+
   const { navigate } = useNavigation();
+
+  async function fetchPools(){
+    try{
+      setIsLoading(true);
+
+      const response = await api.get('/pools');
+      setPools(response.data.pools);
+
+    }catch (error) {
+      toast.show({
+        title: 'Não foi possível carregar os bolões',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    }finally{
+      setIsLoading(false);
+    }
+
+    useEffect(() => {
+      fetchPools();
+    }, [])
+  }
 
   return(
     <VStack flex={1} bgColor="gray.900">
@@ -19,6 +54,19 @@ export function Pools(){
           onPress={ () => navigate('find') }
         />
       </VStack>
+
+    {
+      isLoading ? <Loading/> :
+        <FlatList
+          data={pools}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          ListEmptyComponent={() => <EmptyPoolList />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          px={5}
+        />
+    }
 
     </VStack>
   );
